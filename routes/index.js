75eaea07e912e5cafd9a5acce3a3a62ee9111e62
bloominolive <bloomin.olive88@gmail.com>
2023-09-20 13:58:06 -0400

    project part 2 done

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
+router.use('/', require('./swagger'));
 
-// router.get('/',(req, res) => {
-//     res.send('Hello World');
-// });
+router.get('/', (req, res)=> {
+    //#swagger.tags-['Hello World']
+    res. send('Hello World');
+});
 
 router.use('/users', require('./users'));
+
 module.exports = router;
